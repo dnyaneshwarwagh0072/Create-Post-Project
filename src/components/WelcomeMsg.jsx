f438388f560671ba75f4b2734ea65b19324a26b8
Dnyaneshwar Yadav Wagh <85550894+dnyaneshwarwagh0072@users.noreply.github.com>
2024-03-25 01:40:28 +0530

    Update WelcomeMsg.jsx

--- src/components/WelcomeMsg.jsx
+++ src/components/WelcomeMsg.jsx
@@ -3,7 +3,7 @@ const WelcomeMsg = ({ getPostList }) => {
     return (
         <div className="msg-container msg-css">
             <h2 className="">There are no Posts</h2>
-            <button onClick={getPostList} type="button" className="btn btn-primary">Get the Posts</button>
+            <button onClick={getPostList} type="button" className="btn btn-primary">GET SOME POSTS</button>
         </div>
     )
 }
